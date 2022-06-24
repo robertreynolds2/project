3fad1a3c08984e5dffd8f18c35f9c9bb7e57879a
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-06-24 17:12:16 +0700

    Update c9.sh

--- c9.sh
+++ c9.sh
@@ -1,1 +1,1 @@
-apt update -y && apt install build-essential git nodejs python2.7 -y && git clone https://github.com/c9/core.git c9sdk && cd c9sdk && scripts/install-sdk.sh && nodejs server.js -p 8080 -l 0.0.0.0 -a jack:qwee123
+apt update -y && apt install build-essential git nodejs python2.7 -y && git clone https://github.com/c9/core.git c9sdk && cd c9sdk && scripts/install-sdk.sh && nodejs server.js -p 8080 -l 0.0.0.0 -a update:qwee123
